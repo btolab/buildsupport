00249aee50c03cc0dca8216d800a612df05c2a90
Jeffrey Clark <dude@zaplabs.com>
2015-12-09 09:53:12 -0600

    WIP 0.167

---
+++ build.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Cross-compile SDL version of MAME for Windows and OSX
+#
+
+# error handling setup
+set -o nounset -o pipefail -o errtrace
+
+error() {
+	local lc="$BASH_COMMAND" rc=$?
+	echo "ERROR in $0 : line $1 exit code $2"
+	echo "      [$lc]"
+	exit $2
+}
+trap 'error ${LINENO} ${?}' ERR
+
+# override commands
+function echo() {
+    command echo $(date +"[%d-%h-%Y %H:%M:%S %Z]") "$*"
+}
+function pushd() {
+    command pushd "$@" >/dev/null
+}
+function popd() {
+    command popd "$@" >/dev/null
+}
+
+# Error out if branch is dirty
+if [[ ! -z $(git status --porcelain) ]]; then
+    echo "Repository is not clean, aborting."
+    exit 1
+fi
+
+# Dont reset branch so release script generates proper source archive
+# builtbot should do this anyway.
+#GITREV=$(git rev-parse HEAD)
+#function finish {
+#	git reset --hard ${GITREV}
+#}
+#trap finish EXIT
+
+# apply patches
+echo "Applying buildbot patches"
+git am --signoff ../buildbot/patches/*.patch
+
+echo "Re/Build genie"
+make genie
+pushd 3rdparty/genie/src
+../bin/linux/genie embed >/dev/null
+popd
+make genieclean >/dev/null
+make genie >/dev/null
+
+NPROC=$(getconf _NPROCESSORS_ONLN)
+LLIMIT=$(awk 'BEGIN{printf"%.1f",'${NPROC}'/3}')
+
+echo "Build: ${NPROC} jobs, ${LLIMIT} load limit"
+
+echo Windows 32-bit debug version....
+
+make TARGETOS=windows TOOLCHAIN=i686-w64-mingw32.static- TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=0 DEBUG=1 STRIP_SYMBOLS=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 SDL_LIBVER=sdl2 USE_LIBSDL=1 OSD=sdl -j${NPROC} -l${LLIMIT}
+
+echo Windows 32-bit release version....
+
+make TARGETOS=windows TOOLCHAIN=i686-w64-mingw32.static- TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=0 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 SDL_LIBVER=sdl2 USE_LIBSDL=1 OSD=sdl -j${NPROC} -l${LLIMIT}
+
+echo Windows 64-bit release version....
+
+make TARGETOS=windows TOOLCHAIN=x86_64-w64-mingw32.static- TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 REGENIE=1 SDL_LIBVER=sdl2 USE_LIBSDL=1 OSD=sdl -j${NPROC} -l${LLIMIT}
+
+export MACOSX_DEPLOYMENT_TARGET=10.6
+`osxcross-conf`
+
+echo MACOSX 32-bit release version....
+
+make TARGETOS=macosx CROSS_BUILD=1 TOOLCHAIN=i386-apple-darwin11- TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=0 OPTIMIZE=2 NOWERROR=1 REGENIE=1 USE_SYSTEM_LIB_EXPAT=1 USE_SYSTEM_LIB_ZLIB=1 SDL_LIBVER=sdl2 USE_LIBSDL=1 -j${NPROC} -l${LLIMIT}
+
+echo MACOSX 64-bit release version....
+
+make TARGETOS=macosx CROSS_BUILD=1 TOOLCHAIN=x86_64-apple-darwin11- TARGET=mame TOOLS=1 SEPARATE_BIN=1 PTR64=1 OPTIMIZE=2 NOWERROR=1 REGENIE=1 USE_SYSTEM_LIB_EXPAT=1 USE_SYSTEM_LIB_ZLIB=1 SDL_LIBVER=sdl2 USE_LIBSDL=1 -j${NPROC} -l${LLIMIT}
---
+++ release.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# error handling setup
+set -o nounset -o pipefail -o errtrace
+
+error() {
+	local lc="$BASH_COMMAND" rc=$?
+	echo "ERROR in $0 : line $1 exit code $2"
+	echo "      [$lc]"
+	exit $2
+}
+trap 'error ${LINENO} ${?}' ERR
+
+# override commands
+function echo() {
+    command echo $(date +"[%d-%h-%Y %H:%M:%S %Z]") "$*"
+}
+function pushd() {
+    command pushd "$@" >/dev/null
+}
+function popd() {
+    command popd "$@" >/dev/null
+}
+
+RELEASE=$(git describe --tag --abbrev=0 | sed 's/mame//')
+
+echo Begin packaging SDLMAME ${RELEASE} ...
+
+echo Remove old release directories ...
+rm -rf build/*/release
+rm -rf build/release
+
+# recursively assemble binary packages
+for BUILD in $(ls build/*/bin | grep :\$ | sed 's/.*\/\(.*\)\/bin:$/\1/'); do
+	for ARCH in $(ls build/${BUILD}/bin); do
+		for TYPE in $(ls build/${BUILD}/bin/${ARCH}); do
+			echo Assembling $BUILD $ARCH $TYPE
+			RELEASENAME="sdlmame${RELEASE}b"
+			if [[ ${ARCH} == x64 ]]; then
+				RELEASENAME="${RELEASENAME}_64bit"
+			fi
+			if [[ ${TYPE} == Debug ]]; then
+				RELEASENAME="${RELEASENAME}_debug"
+			fi
+			PACKAGEDIR="`pwd`/build/${BUILD}/release/${ARCH}/${TYPE}/package"
+			BUILDDIR="`pwd`/build/${BUILD}/bin/${ARCH}/${TYPE}"
+			mkdir -p ${PACKAGEDIR}
+			pushd ${BUILDDIR}
+			[ -f *.sym ] && cp *.sym ${PACKAGEDIR}/
+			find . -executable -type f -exec cp "{}" ${PACKAGEDIR} \;
+			popd
+			cp ../build/whatsnew/whatsnew_${RELEASE}.txt ${PACKAGEDIR}/whatsnew.txt
+			cp -r docs hash web nl_examples ${PACKAGEDIR}/
+			7za x ../build/mamedirs.zip -o${PACKAGEDIR}/ >/dev/null
+			echo Packaging ${BUILD} ${ARCH} ${TYPE}
+			pushd ${PACKAGEDIR}
+			if [[ ${BUILD} == mingw* ]] || [[ ${BUILD} == vs* ]]; then
+				7za a -mx=9 -y -r -t7z -sfx7zWin32.sfx ../${RELEASENAME}.exe >/dev/null
+			else
+				7za a -mpass=4 -mfb=255 -y -r -tzip ../${RELEASENAME}.zip >/dev/null
+			fi
+			popd
+		done
+	done
+done
+
+#echo Assembling release source ....
+#mkdir -p build/release/src
+#git archive HEAD | tar -x -C build/release/src
+#pushd build/release/src
+#echo Creating 7zip self-extracting source archive....
+#7za a -mx=9 -y -r -t7z -sfx7zWin32.sfx ../sdlmame${RELEASE}s.exe >/dev/null
+#echo Creating raw source ZIP....
+#7za a -mx=0 -y -r -tzip ../sdlmame.zip >/dev/null
+#echo Creating final source ZIP....
+#7za a -mpass=4 -mfb=255 -y -tzip ../sdlmame${RELEASE}s.zip ../sdlmame.zip  >/dev/null
+#rm ../sdlmame.zip
+#popd
+
+echo Finished creating release....
